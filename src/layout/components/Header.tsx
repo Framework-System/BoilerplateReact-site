f104257ab14dc60cad1db1a94a6357897c4ceef5
Rafael Gontijo <rafaelgontijowinter@gmail.com>
2024-11-13 19:30:40 -0300

    Refatora componente Header para exibir informações do usuário e melhorar a lógica do dropdown

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -24,42 +24,45 @@ const Header = () => {
             <h1 className="text-2xl font-semibold">Gestão de vagas</h1>
           </div>
         </div>
-        <div className="basis-1/3">
-          <div className="flex px-5 justify-end">
-            <button
-              type="button"
-              className="p-2 hover:bg-[#533961] rounded-full"
-            >
-              <Bell className="w-5 h-5" />
-            </button>
-            <div className="px-2">
+        {user &&
+          <div className="basis-1/3">
+            <div className="flex px-5 justify-end">
               <button
                 type="button"
-                onClick={() => setShowDropdown(!showDropdown)}
-                className="flex items-center space-x-2 focus:outline-none"
+                className="p-2 hover:bg-[#533961] rounded-full"
               >
-                <div className="w-8 h-8 bg-purple-300 rounded-full flex items-center justify-center">
-                  <span className="text-[#432B4F] font-medium">
-                    {user?.name.charAt(0)}
-                  </span>
-                </div>
-                <span>{user?.name}</span>
+                <Bell className="w-5 h-5" />
               </button>
-              {showDropdown && (
-                <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
-                  <button
-                    type="button"
-                    onClick={handleLogout}
-                    className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full"
-                  >
-                    <LogOut className="w-4 h-4 mr-2" />
-                    Sair
-                  </button>
-                </div>
-              )}
+              <div className="px-2">
+                <button
+                  type="button"
+                  onClick={() => setShowDropdown(!showDropdown)}
+                  className="flex items-center space-x-2 focus:outline-none"
+                >
+                  <div className="w-8 h-8 bg-purple-300 rounded-full flex items-center justify-center">
+                    <span className="text-[#432B4F] font-medium">
+                      {user?.name.charAt(0)}
+                    </span>
+                  </div>
+                  <span>{user?.name}</span>
+                </button>
+                {showDropdown && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
+                    <button
+                      type="button"
+                      onClick={handleLogout}
+                      className="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full"
+                    >
+                      <LogOut className="w-4 h-4 mr-2" />
+                      Sair
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        }
+
       </div>
     </header>
   );
