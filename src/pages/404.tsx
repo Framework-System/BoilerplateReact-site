6a3ccd8c36f13b9c88b86ad687c0b52b54137eee
Rafael Gontijo <rafaelgontijowinter@gmail.com>
2024-11-15 20:10:17 -0300

    Ajusta estilos da página 404 para melhorar a apresentação e remove a funcionalidade de navegação

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,10 @@
-import { useNavigate } from 'react-router-dom';
 import styles from './404.module.css';
 
 const Page404 = () => {
-  const navigate = useNavigate();
-
-  const handleBackToHome = () => navigate('/');
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>404</h1>
         <p className={styles.description}>Oops! Página não encontrada</p>
         <p className={styles.subDescription}>
           A página que procura pode ter sido removida, o seu nome alterado ou estar temporariamente indisponível.
@@ -19,13 +14,6 @@ const Page404 = () => {
           alt="404 illustration"
           className={styles.image}
         />
-        <button
-          type="button"
-          onClick={handleBackToHome}
-          className={styles.button}
-        >
-          Volte
-        </button>
       </div>
     </div>
   );
